a5372c8e262f500eabc8d2ba4950d199b5272946
genboy <msg@genboy.net>
2019-04-16 22:47:30 +0200

    Translation encode json utf-8 fixed

--- src/genboy/Festival/lang/Language.php
+++ src/genboy/Festival/lang/Language.php
@@ -1,36 +1,22 @@
 <?php declare(strict_types = 1);
 
 /** src/genboy/Festival/lang/Language.php */
-
 namespace genboy\Festival\lang;
 
 class Language{
-
 	public static $instance;
-
 	public function __construct($owner, $langjson){
-
 		$this->owner = $owner;
 		$this->trans = $langjson;
 		self::$instance = $this;
-
 	}
-
 	static function translate($key){
-
-
-
-		$txt_code = self::$instance->trans[$key];
-
-        $txt = utf8_decode( $txt_code );
-
+		$txt = self::$instance->trans[$key];
 		if (strpos($txt, "%n") != false) {
 			$text = str_replace("%n", "\n", $txt);
 		} else {
 			$text = $txt;
 		}
 		return $text;
-
 	}
-
 }
